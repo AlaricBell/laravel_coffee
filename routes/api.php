4f1a4d8bfbe655c4cb9752f4d72aa4e3cd650b0f
Alaric Bell <alaric.bell@gmail.com>
2021-08-24 17:29:23 +0200

    Added sanctum for authentication

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CoffeeController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +16,23 @@ use App\Http\Controllers\CoffeeController;
 |
 */
 
+
+// public routes
 Route::get('/coffees', [CoffeeController::class, 'index'])->name('coffees');
-
 Route::get('/coffees/search/{slug}', [CoffeeController::class, 'search'])->name('coffee-search');
-
 Route::get('/coffee/{id}', [CoffeeController::class, 'show'])->name('coffee');
 
-Route::post('/coffee', [CoffeeController::class, 'store'])->name('coffee');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::put('/coffee/{id}', [CoffeeController::class, 'update'])->name('coffee');
+// Auth protected routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/coffee', [CoffeeController::class, 'store'])->name('coffee');
+    Route::put('/coffee/{id}', [CoffeeController::class, 'update'])->name('coffee');
+    Route::delete('/coffee/{id}', [CoffeeController::class, 'destroy'])->name('coffee');
 
-Route::delete('/coffee/{id}', [CoffeeController::class, 'destroy'])->name('coffee');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
